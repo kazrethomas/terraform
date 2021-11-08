6ec850baf24f0c84ae3b0462f7e37bfa724ac9fe
Kazre Thomas <kazrethomas@Kazres-MacBook-Pro.local>
2021-11-08 16:07:39 -0500

    first commit

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+project                  = "apple-com"
+credentials_file         = "/Users/kazrethomas/downloads/gothic-state-331515-92d2c10e0601.json"
---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "project" {
+  default = "gothic-state-331515"
+}
+
+variable "credentials_file" { }
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-c"
+}
+
+
+variable "network_name" {
+  default = "terraform-network"
+}
